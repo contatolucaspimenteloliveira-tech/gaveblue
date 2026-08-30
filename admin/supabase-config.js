141ddddac0a2afb52ee1d9a148969734c583b987
gaveblue <contato.lucaspimenteloliveira@gmail.com>
2026-08-30 08:09:40 -0300

    Atualiza chave publica do painel Supabase

--- admin/supabase-config.js
+++ admin/supabase-config.js
@@ -1,5 +1,5 @@
 window.GAVEBLUE_SUPABASE_CONFIG = Object.freeze({
   url: 'https://wkssfugzghwifaddagfr.supabase.co',
-  anonKey: 'sb_publishable_ltaNA7nnVozoSCOcZIjg',
+  anonKey: 'sb_publishable_XuQH9K6Lu7dfUEq_pBpWpQ_89PY2_Xl',
   functionName: 'platform-admin'
 });
